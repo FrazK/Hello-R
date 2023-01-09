6fe1c9d289e095a9570bd01219b650bba2836d11
FrazK <frazkhan813@gmail.com>
2023-01-08 16:26:25 -0800

    few lines added

--- testing.r
+++ testing.r
@@ -1,8 +1,10 @@
 # This is a testing file for R programming.
 print("Hello from git ")
 
-name = "fraz"
-version="Rstudio"
+name="fraz"
+version="R-studio in writing"
+
 print(name)
-print("These lines has been written in RStudio")
+print("These lines are written in R studion")
+print("These are some other new lines that im writing bofre commit")
 
